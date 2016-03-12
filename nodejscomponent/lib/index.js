e0c2362e8cc09f5ce27941f5d1c8ac1fc990c8df
goto63 <tgoto63@gmail.com>
2016-03-12 23:50:21 +0900

    change param name

--- nodejscomponent/lib/index.js
+++ nodejscomponent/lib/index.js
@@ -3,13 +3,11 @@
  */
 
 module.exports.respond = function(event, cb) {
-  console.log("event : " + JSON.stringify(event));
-
   var response = {
     message: "Your Serverless function ran successfully!"
   };
 
   setTimeout(function() {
     return cb(null, response);
-  }, event.time);
+  }, event.delay);
 };
